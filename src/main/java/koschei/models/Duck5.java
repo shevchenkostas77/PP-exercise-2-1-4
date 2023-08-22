9938b22abd5bc4a869d083c06662601d0cc4da7c
shevchenkostas77 <shevchenkostas77@icloud.com>
2023-08-22 15:02:22 +0300

    solution

---
+++ src/main/java/koschei/models/Duck5.java
@@ -0,0 +1,17 @@
+package koschei.models;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Duck5 {
+    private final Egg6 egg;
+
+    public Duck5(Egg6 egg6) {
+        this.egg = egg6;
+    }
+
+    @Override
+    public String toString() {
+        return ", в утке яйцо " + egg.toString();
+    }
+}
